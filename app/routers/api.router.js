0b08e4f953908cc0396d0198929aa87895463a89
Seb Smith <seb-smith@live.com>
2020-05-05 16:46:49 +0100

    Accidentally moved a couple folders and committed, fixed it and started creating an endpoint for topics

--- app/routers/api.router.js
+++ app/routers/api.router.js
@@ -1,7 +1,10 @@
 const apiRouter = require("express").Router();
+const topicsRouter = require("./");
 
 apiRouter.get("/", (req, res, next) => {
   res.status(200).send({ msg: "Welcome to NC NEWS!" });
 });
+
+apiRouter.use("/topics", topicsRouter);
 
 module.exports = apiRouter;
